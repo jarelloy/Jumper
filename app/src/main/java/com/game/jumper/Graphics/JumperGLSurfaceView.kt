e71daa83b62afb63999116e196cf0450c897dfeb
matth3w03 <rollymatthew0513@gmail.com>
2023-02-14 15:13:44 +0800

    modified game and gameloop to integrate with renderer & surface view

--- app/src/main/java/com/game/jumper/Graphics/JumperGLSurfaceView.kt
+++ app/src/main/java/com/game/jumper/Graphics/JumperGLSurfaceView.kt
@@ -3,18 +3,26 @@ package com.game.jumper.Graphics
 import android.content.Context
 import android.opengl.GLSurfaceView
 import android.util.AttributeSet
+import com.game.jumper.GameLoopGl
 
 open class JumperGLSurfaceView : GLSurfaceView {
 
     var renderer : JumperGLRenderer
+    private lateinit var gameLoop : GameLoopGl
 
     constructor(context: Context) : super(context) {
+        val surfaceHolder = holder
+        surfaceHolder.addCallback(this)
         renderer = JumperGLRenderer()
+        gameLoop = GameLoopGl(renderer, surfaceHolder)
         initOpenGLView()
     }
 
     constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        val surfaceHolder = holder
+        surfaceHolder.addCallback(this)
         renderer = JumperGLRenderer()
+        gameLoop = GameLoopGl(renderer, surfaceHolder)
         initOpenGLView()
     }
 
--- app/src/main/java/com/game/jumper/MainActivity.kt
+++ app/src/main/java/com/game/jumper/MainActivity.kt
@@ -7,16 +7,19 @@ import android.view.WindowInsets
 import android.view.WindowInsetsController
 import android.view.WindowManager
 import com.game.jumper.databinding.ActivityMainBinding
+import com.game.jumper.Graphics.JumperGLSurfaceView
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var game : Game
+    private lateinit var game : GameGl
+    private lateinit var glSurfaceView : JumperGLSurfaceView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        game = Game(this)
+        game = GameGl(this)
+        glSurfaceView = JumperGLSurfaceView(this)
         setContentView(game)
         //setContentView(binding.root)
 
