d5de63bdd7b85334698633c4cf866a9ca3dc6f0d
matth3w03 <rollymatthew0513@gmail.com>
2023-02-16 16:43:19 +0800

    Removed game and gameloop

--- app/src/main/java/com/game/jumper/MainActivity.kt
+++ app/src/main/java/com/game/jumper/MainActivity.kt
@@ -8,12 +8,12 @@ import com.game.jumper.engine.GameGl
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    //private lateinit var binding: ActivityMainBinding
     private lateinit var game : GameGl
     private lateinit var glSurfaceView : JumperGLSurfaceView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
 
         game = GameGl(this)
         glSurfaceView = JumperGLSurfaceView(this)
