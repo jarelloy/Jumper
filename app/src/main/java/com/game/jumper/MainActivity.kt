0aaa459628bb70976279ac42f21312127ceb6c0f
jarelloy <jarell.sit@gmail.com>
2023-02-26 17:05:12 +0800

    Update MainActivity.kt

--- app/src/main/java/com/game/jumper/MainActivity.kt
+++ app/src/main/java/com/game/jumper/MainActivity.kt
@@ -1,3 +1,19 @@
+/*!*****************************************************************************
+\file			MainActivity.kt
+\author			Jarell Ow Yong
+\par DP email:	o.yongjarell@digipen.edu
+\par Course:	CSD3156
+\par Project:	Jumper Game
+\date			25/2/2023
+\brief
+
+This file contains the function definition used in Jumper for MainActivity
+
+\copyright	Copyright (C) 2021 DigiPen Institute of Technology.
+			Reproduction or disclosure of this file or its contents without the
+			prior written consent of DigiPen Institute of Technology is prohibited.
+*******************************************************************************/
+
 package com.game.jumper
 
 import android.content.Intent
@@ -16,6 +32,13 @@ class MainActivity : AppCompatActivity() {
 //    private lateinit var game : GameGl
 //    private lateinit var glSurfaceView : JumperGLSurfaceView
 
+    /*
+    onCreate function override to be called when first starting the activity
+    This function uses the activity_main.xml file for the necessary layout.
+    This function creates the main menu screen for the game which user can navigate through each
+    different other activity. This is the main activity as user will be first brought into this
+    screen before accessing any other sub-activities
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,31 +46,32 @@ class MainActivity : AppCompatActivity() {
         //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(binding.root)
 
+        //Button for play game
         binding.playgameBtn.setOnClickListener {
-            //game = GameGl(this)
-            //glSurfaceView = JumperGLSurfaceView(this)
-            //setContentView(game)
-
             val intent = Intent(this, GameActivity::class.java)
             startActivity(intent)
         }
 
+        //Button for instruction menu
         binding.howtoplayBtn.setOnClickListener {
             val intent = Intent (this, InstructionActivity::class.java)
             startActivity(intent)
         }
 
+        //Button for scoreboard
         binding.scoreboardBtn.setOnClickListener {
             val intent = Intent (this, ScoreboardActivity::class.java)
             startActivity(intent)
         }
 
+        //Button for cosmetics
         binding.customizeplayerBtn.setOnClickListener {
             val intent = Intent (this, CustomizePlayerActivity::class.java)
             startActivity(intent)
         }
         //setContentView(binding.root)
 
+        //Button to exit the game
         binding.exitgameBtn.setOnClickListener {
             finishAffinity()
             exitProcess(0)
